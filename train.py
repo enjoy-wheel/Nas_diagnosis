c21289e4c9edee898c785f62fa4feee881963910
Jinyuan <1095481875@qq.com>
2024-11-05 23:44:50 +0800

    11.5

--- train.py
+++ train.py
@@ -6,6 +6,9 @@ import data_loader.data_loaders as module_data
 import model.loss as module_loss
 import model.metric as module_metric
 import model.model as module_arch
+import model.enas.enas_controller as controller_arch
+import model.enas.shared_cnn as shared_cnn_arch
+import model.enas.shared_rnn as shared_cnn_arch
 from parse_config import ConfigParser
 from trainer import Trainer
 from utils import prepare_device
@@ -31,7 +34,8 @@ def main(config):
 
     # 根据配置文件中关于模型架构的设置，初始化模型实例
     # build model architecture, then print to console
-    model = config.init_obj('arch', module_arch)
+    shared_model = config.init_obj('arch', module_arch)
+    controller_model = config.init_obj('arch', )
     # 将模型的结构信息记录到日志中，便于查看和验证模型架构
     logger.info('regularizing:')
     for regularizer in [('activation regularization',
@@ -42,32 +46,35 @@ def main(config):
                          config.config["PTB_regularizations"]["norm_stabilizer_regularization"]["state"])]:
         if regularizer[1]:
             logger.info(f'{regularizer[0]}')
-    logger.info(model)
-
+    logger.info(shared_model)
+    logger.info(controller_model)
     # 根据配置文件中指定的 GPU 数量，准备训练设备
     # prepare for (multi-device) GPU training
     device, device_ids = prepare_device(config['n_gpu'])
-    model = model.to(device)
+    shared_model = shared_model.to(device)
+    controller_model = controller_model.to(device)
     # 如果使用多个 GPU，则使用 DataParallel 将模型并行分布到多个 GPU 上，以加速训练过程
     if len(device_ids) > 1:
-        model = torch.nn.DataParallel(model, device_ids=device_ids)
-
+        shared_model = torch.nn.DataParallel(shared_model, device_ids=device_ids)
+        controller_model = torch.nn.DataParallel(controller_model, device_ids=device_ids)
     # 获取损失函数和评估指标
     # get function handles of loss and metrics
     criterion = getattr(module_loss, config['loss'])
     metrics = [getattr(module_metric, met) for met in config['metrics']]
-
     # 构建优化器和学习率调度器
     # build optimizer, learning rate scheduler. delete every lines containing lr_scheduler for disabling scheduler
     # 过滤出模型中需要更新的参数（requires_grad = True），有些模型参数可能被冻结（如预训练模型的某些层），不需要在训练中更新
-    trainable_params = filter(lambda p: p.requires_grad, model.parameters())
+    shared_trainable_params = filter(lambda p: p.requires_grad, shared_model.parameters())
+    controller_trainable_params = filter(lambda p: p.requires_grad, controller_model.parameters())
     # 使用配置对象初始化优化器
-    optimizer = config.init_obj('optimizer', torch.optim, trainable_params)
+    shared_optimizer = config.init_obj('shared_optimizer', torch.optim, shared_trainable_params)
+    controller_optimizer = config.init_obj('controller_optimizer', torch.optim, controller_trainable_params)
     # 使用配置对象初始化学习率调度器
-    lr_scheduler = config.init_obj('lr_scheduler', torch.optim.lr_scheduler, optimizer)
+    shared_lr_scheduler = config.init_obj('lr_scheduler', torch.optim.lr_scheduler, shared_optimizer)
+    controller_lr_scheduler = config.init_obj('lr_scheduler', torch.optim.lr_scheduler, controller_optimizer)
 
     # 创建训练器Trainer实例，并开始训练
-    trainer = Trainer(model, criterion, metrics, optimizer,
+    trainer = Trainer(shared_model, controller_model, criterion, metrics, shared_optimizer, controller_optimizer,
                       config=config,
                       device=device,
                       data_loader=data_loader,
